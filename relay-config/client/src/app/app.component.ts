75452eaea67b38cd671151d2f75da0434a0c25c9
zyarr-technologies <zyarr.technologies@gmail.com>
2023-11-15 18:46:26 +0530

    Electron App

---
+++ relay-config/client/src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { AppService } from './app.service';
+import { IPAddressType, RelayConfigType } from './app.model'; import { IpcRendererEvent } from 'electron';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client';
+  isConnected = false;
+
+  connectionConfig = this.formBuilder.group({
+    ipaddress: ['', [Validators.required, Validators.pattern('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')]],
+    port: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(5), Validators.pattern("[0-9]*")]],
+  });
+
+  relayConfig = this.formBuilder.group({
+    relay1: 'off',
+    relay2: 'off',
+    relay3: 'off',
+    relay4: 'off'
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private readonly ipc: AppService) {
+
+    this.relayConfig.disable()
+  }
+
+  get ipaddress(): FormControl {
+    return this.connectionConfig.get("ipaddress") as FormControl;
+  }
+
+  get port(): FormControl {
+    return this.connectionConfig.get("port") as FormControl;
+  }
+
+  private controlOnConnection() {
+    this.isConnected = true;
+    this.relayConfig.enable()
+    this.connectionConfig.get("ipaddress")?.disable();
+    this.connectionConfig.get("port")?.disable();
+  }
+
+  private controlOnDisconnection() {
+    this.isConnected = false;
+    this.relayConfig.disable()
+    this.connectionConfig.get("ipaddress")?.enable();
+    this.connectionConfig.get("port")?.enable();
+  }
+
+  onOpenConnection() {
+    let data = this.connectionConfig.value as IPAddressType
+    this.ipc.send('connect', JSON.stringify(data));
+    this.controlOnConnection();
+    console.log("Connected............")
+  }
+
+  onCloseConnection() {
+    let data = this.connectionConfig.value as IPAddressType
+    this.ipc.send('disconnect', JSON.stringify(data));
+    this.controlOnDisconnection();
+    console.log("Disconnected............")
+  }
+
+  onRelayConfig() {
+    let data = this.relayConfig.value as RelayConfigType
+    this.ipc.send('relayconfig', JSON.stringify(data));
+  }
+}
+
